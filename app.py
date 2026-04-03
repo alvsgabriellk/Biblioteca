a7d05cb84f3f1c7747a130753ca11a8ee5d3d798
alvsgabriellk <cauagabriell81@gmail.com>
2026-04-03 09:46:17 -0300

    Rotas e redicerionamento de dados de usuario sendo passados

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 from database.databanco import db
 import os
 from dotenv import load_dotenv
 from models import Usuario, Livro, Emprestimo, StatusEmprestimo
+from routes import usuario_bp
 
 load_dotenv()
 
@@ -11,6 +12,14 @@ app.secret_key = os.getenv("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 
 db.init_app(app)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+app.register_blueprint(usuario_bp, url_prefix="/usuarios")
 
 with app.app_context():
     db.create_all()
