7e4be570762b6b64572255018641198854cd8252
alvsgabriellk <cauagabriell81@gmail.com>
2026-04-02 22:40:20 -0300

    Criando dependências do app com flask e banco

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from database.databanco import db
+import os
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+if __name__ == "__main__":
+    app.run(debug=True)
