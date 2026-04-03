037768279896af97f7424d8d8842f581fa6196de
alvsgabriellk <cauagabriell81@gmail.com>
2026-04-03 11:09:13 -0300

    Adicionando rota POST para criar livro pro banco

--- routes/livro_routes.py
+++ routes/livro_routes.py
@@ -4,3 +4,27 @@ from database.databanco import db
 from sqlalchemy.exc import IntegrityError
 
 livro_bp = Blueprint("livros", __name__)
+
+@livro_bp.route("/novo", methods=["POST"])
+def novo_livro():
+    titulo = request.form.get("titulo")
+    autor = request.form.get("autor")
+    quantidade_total = request.form.get(int("quantidade_total"))
+    
+    livro = Livro(
+        titulo=titulo,
+        autor=autor,
+        quantidade_total=quantidade_total,
+        quantidade_disponivel=quantidade_total
+    )
+
+    try:
+        db.session.add(livro)
+        db.session.commit()
+    except IntegrityError:
+        db.session.rollback()
+        flash("Esse Livro já foi cadastrado.", "error")
+        return redirect(url_for("index"))
+    
+    flash("Livro criado com sucesso!", "success")
+    return redirect(url_for("index"))
