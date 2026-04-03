6644a6543c047d2f21f300a9b42acd037f47e632
alvsgabriellk <cauagabriell81@gmail.com>
2026-04-03 09:45:07 -0300

    Adicionando rota DELETE pra usuario. E alterações de requisições.

--- routes/usuario_routes.py
+++ routes/usuario_routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, redirect, url_for, render_template, flash
 from models import Usuario
 from database.databanco import db
 from sqlalchemy.exc import IntegrityError
+from werkzeug.security import generate_password_hash
 
 usuario_bp = Blueprint("usuarios", __name__)
 
@@ -12,10 +13,16 @@ def cadastrar_usuario():
     email = request.form.get("email")
     senha = request.form.get("senha")
 
+    if not nome or not email or not senha:
+        flash("Todos os dados são obrigatórios", "error")
+        return redirect(url_for("index"))
+
+    senha_hash = generate_password_hash(senha)
+
     usuario = Usuario(
         nome=nome,
         email=email,
-        senha=senha
+        senha=senha_hash
     )
 
     try:
@@ -23,8 +30,24 @@ def cadastrar_usuario():
         db.session.commit()
     except IntegrityError:
         db.session.rollback()
-        flash("Esse e-mail já foi cadastrado.")
-        return redirect("/")
+        flash("Esse e-mail já foi cadastrado.", "error")
+        return redirect(url_for("index"))
 
-    flash("Usúario criado com sucesso!")
-    return render_template("index.html")        
+    flash("Usúario criado com sucesso!", "success")
+    return redirect(url_for("index"))     
+
+@usuario_bp.route("/deletar", methods=["POST"])
+def deletar_usuario():
+    id = request.form.get("usuario_id")
+
+    if not id:
+        flash("ID do Usúario é obrigatório", "error")
+        return redirect(url_for("index"))
+
+    usuario = Usuario.query.get(id)
+
+    db.session.delete(usuario)
+    db.session.commit()
+
+    flash("Usúario deletado com sucesso!", "success")
+    return redirect(url_for("index"))
