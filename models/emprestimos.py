6b0a6aa749d86d028f9a04f95b9812fcf702d39e
alvsgabriellk <cauagabriell81@gmail.com>
2026-04-02 23:54:04 -0300

    Erro de chamada de tabela encontrada

--- models/emprestimos.py
+++ models/emprestimos.py
@@ -13,8 +13,8 @@ class Emprestimo(db.Model):
     __tablename__ = "emprestimos"
 
     id = db.Column(db.Integer, primary_key=True)
-    usuario_id = db.Column(db.Integer, db.ForeignKey("usuario.id"), unique=True)
-    livro_id = db.Column(db.Integer, db.ForeignKey("livro.id"), unique=True)
+    usuario_id = db.Column(db.Integer, db.ForeignKey("usuarios.id"), unique=True)
+    livro_id = db.Column(db.Integer, db.ForeignKey("livros.id"), unique=True)
     data_emprestimo = db.Column(db.Date, default=date.today)
     data_devolucao = db.Column(db.Date, default=lambda: date.today() + timedelta(days=7))
     status = db.Column(
