43f8612ec2639502d4c65490b15837e3f2836497
alvsgabriellk <cauagabriell81@gmail.com>
2026-04-03 00:24:24 -0300

    Criando rota POST para criar o usuario

--- routes/usuario_routes.py
+++ routes/usuario_routes.py
@@ -4,3 +4,27 @@ from database.databanco import db
 from sqlalchemy.exc import IntegrityError
 
 usuario_bp = Blueprint("usuarios", __name__)
+
+@usuario_bp.route("/cadastrar", methods=["POST"])
+def cadastrar_usuario():
+
+    nome = request.form.get("nome")
+    email = request.form.get("email")
+    senha = request.form.get("senha")
+
+    usuario = Usuario(
+        nome=nome,
+        email=email,
+        senha=senha
+    )
+
+    try:
+        db.session.add(usuario)
+        db.session.commit()
+    except IntegrityError:
+        db.session.rollback()
+        flash("Esse e-mail já foi cadastrado.")
+        return redirect("/")
+
+    flash("Usúario criado com sucesso!")
+    return render_template("index.html")        
