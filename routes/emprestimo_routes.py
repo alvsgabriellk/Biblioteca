4dd39e049f1ea8b4e6c26cfa3a3edcb9d549d010
alvsgabriellk <cauagabriell81@gmail.com>
2026-04-09 11:55:34 -0300

    Rota adicionada em emprestimos POST: Devolver Livro

--- routes/emprestimo_routes.py
+++ routes/emprestimo_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, render_template, flash
-from models import Emprestimo, Livro, Usuario
+from models import Emprestimo, Livro, Usuario, StatusEmprestimo
 from database.databanco import db
 from sqlalchemy.exc import IntegrityError
 
@@ -58,3 +58,49 @@ def realizar_emprestimo():
     flash("Empréstimo realizado com sucesso!", "success")
     return render_template("index.html"), 201
 
+@emprestimo_bp.route("/devolver", methods=["POST"])
+def devolver_livro():
+    livro_id = request.form.get("livro_id", "").strip()
+
+    if not livro_id:
+        flash("Todos os dados sõa obrigatórios", "error")
+        return render_template("index.html"), 400
+    
+    try:
+        livro_id = int(livro_id)
+    except (TypeError, ValueError):
+        flash("ID inválido", "error")
+        return render_template("index.html"), 400
+    
+    if not livro_id.isdigit():
+        flash("ID tem que ser um número!", "error")
+        return render_template("index.html"), 400
+    
+    if livro_id <= 0:
+        flash("ID inválido", "error")
+        return render_template("index.html"), 400
+    
+    emprestimo = db.session.get(Emprestimo, livro_id)
+
+    if not emprestimo:
+        flash("Empréstimo não encontrado no sistema", "error")
+        return render_template("index.html"), 404
+    
+    if emprestimo.status == StatusEmprestimo.DEVOLVIDO:
+        flash("Livro já foi devolvido", "error")
+        return render_template("index.html"), 409
+    
+    livro = db.session.get(Livro, livro_id)
+
+    if not livro:
+        flash("Livro não encontrado no sistema", "error")
+        return render_template("index.html"), 404
+    
+    emprestimo.status = StatusEmprestimo.DEVOLVIDO
+    
+    livro.quantidade_disponivel += 1
+
+    db.session.commit()
+
+    flash("Livro devolvido com sucesso!", "success")
+    return render_template("index.html"), 200
