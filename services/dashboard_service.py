e83736c9df15107da9f6ade7c1d12c85902a9d67
alvsgabriellk <cauagabriell81@gmail.com>
2026-04-10 10:57:38 -0300

    Criando dado visual de totais livros e usuarios para a pagina html

---
+++ services/dashboard_service.py
@@ -0,0 +1,17 @@
+from sqlalchemy import select, func
+from database.databanco import db
+from models import Livro, Usuario, Emprestimo, StatusEmprestimo
+
+def get_totais_dashboard():
+    total_livros = db.session.execute(
+        select(func.count()).select_from(Livro)
+    ).scalar()
+
+    total_usuarios = db.session.execute(
+        select(func.count()).select_from(Usuario)
+    ).scalar()
+
+    return {
+        "total_livros": total_livros or 0,
+        "total_usuarios": total_usuarios or 0
+    }
