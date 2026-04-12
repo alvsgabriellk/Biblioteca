172c69ae8207c85d933adc5f43236d934281174a
alvsgabriellk <cauagabriell81@gmail.com>
2026-04-12 17:09:02 -0300

    Passando novos dados de funções e rotas de tela para página html

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from database.databanco import db
 import os
 from dotenv import load_dotenv
 from models import Usuario, Livro, Emprestimo, StatusEmprestimo
 from routes import usuario_bp, livro_bp, emprestimo_bp
-from services import get_totais_dashboard, dados_usuario, data_primeiro_criado, totais_livros_quantidade, dados_livro, dados_emprestimo, total_livros_disponiveis
+from services import get_totais_dashboard, dados_usuario, data_primeiro_criado, totais_livros_quantidade, dados_livro, dados_emprestimo, total_livros_disponiveis, total_emprestimos_ativos_e_atrasados
 
 load_dotenv()
 
@@ -17,6 +17,8 @@ db.init_app(app)
 
 @app.route("/")
 def index():
+    open_section = request.args.get("open")
+
     dados_totais = get_totais_dashboard()
     dados_usuarios = dados_usuario()
     dados_primeiro_usuario = data_primeiro_criado()
@@ -24,7 +26,8 @@ def index():
     dados_livros = dados_livro()
     dados_emprestimos = dados_emprestimo()
     dados_total_disponivel = total_livros_disponiveis()
-    return render_template("index.html", **dados_totais, **dados_usuarios, **dados_primeiro_usuario, **dados_totais_livros, **dados_livros, **dados_emprestimos, **dados_total_disponivel)
+    dados_total_ativo_atrasado = total_emprestimos_ativos_e_atrasados()
+    return render_template("index.html", **dados_totais, **dados_usuarios, **dados_primeiro_usuario, **dados_totais_livros, **dados_livros, **dados_emprestimos, **dados_total_disponivel, **dados_total_ativo_atrasado, open_section=open_section)
 
 
 app.register_blueprint(usuario_bp, url_prefix="/usuarios")
