68bacfba637f2985f1316af1a3742f9b6072a1da
alvsgabriellk <cauagabriell81@gmail.com>
2026-04-14 09:13:25 -0300

    Melhorando arquitetura e sintaxes de códigos

--- app.py
+++ app.py
@@ -4,7 +4,18 @@ import os
 from dotenv import load_dotenv
 from models import Usuario, Livro, Emprestimo, StatusEmprestimo
 from routes import usuario_bp, livro_bp, emprestimo_bp
-from services import get_totais_dashboard, dados_usuario, data_primeiro_criado, totais_livros_quantidade, dados_livro, dados_emprestimo, total_livros_disponiveis, total_emprestimos_ativos_e_atrasados, ultimo_emprestimo, atividades_recentes
+from services import (
+    get_totais_dashboard, 
+    dados_usuario, 
+    data_primeiro_criado, 
+    totais_livros_quantidade, 
+    dados_livro, 
+    dados_emprestimo, 
+    total_livros_disponiveis, 
+    total_emprestimos_ativos_e_atrasados, 
+    ultimo_emprestimo, 
+    atividades_recentes
+)
 
 load_dotenv()
 
@@ -19,17 +30,27 @@ db.init_app(app)
 def index():
     open_section = request.args.get("open")
 
-    dados_totais = get_totais_dashboard()
+    dados_emprestimos = dados_emprestimo()
     dados_usuarios = dados_usuario()
+    dados_livros = dados_livro()
+
+    dados_totais = get_totais_dashboard()
     dados_primeiro_usuario = data_primeiro_criado()
     dados_totais_livros = totais_livros_quantidade()
-    dados_livros = dados_livro()
-    dados_emprestimos = dados_emprestimo()
     dados_total_disponivel = total_livros_disponiveis()
     dados_total_ativo_atrasado = total_emprestimos_ativos_e_atrasados()
     dados_ultimo_emprestimo = ultimo_emprestimo()
     dados_atividades = atividades_recentes()
-    return render_template("index.html", **dados_totais, **dados_usuarios, **dados_primeiro_usuario, **dados_totais_livros, **dados_livros, **dados_emprestimos, **dados_total_disponivel, **dados_total_ativo_atrasado,**dados_ultimo_emprestimo,**dados_atividades ,open_section=open_section)
+
+    return render_template(
+        "index.html", 
+        **dados_totais, **dados_usuarios, 
+        **dados_primeiro_usuario, **dados_totais_livros, 
+        **dados_livros, **dados_emprestimos, 
+        **dados_total_disponivel, **dados_total_ativo_atrasado,
+        **dados_ultimo_emprestimo,**dados_atividades,
+        open_section=open_section
+    )
 
 
 app.register_blueprint(usuario_bp, url_prefix="/usuarios")
