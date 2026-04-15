a56b0beeac0821a2a958f1adc637b9e633287911
alvsgabriellk <cauagabriell81@gmail.com>
2026-04-14 22:54:34 -0300

    Tábela para espectador criado

---
+++ models/espectador.py
@@ -0,0 +1,10 @@
+from database.databanco import db
+
+class Espectador(db.Model):
+    __tablename__ = "espectador"
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    senha = db.Column(db.String(20), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
