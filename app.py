1bbbc40bafb912bffb368bbf1d0cfe3fd0ebcb22
alvsgabriellk <cauagabriell81@gmail.com>
2026-04-15 12:05:06 -0300

    Atualizando app
    Criando dependência

--- app.py
+++ app.py
@@ -86,4 +86,6 @@ app.register_blueprint(login_bp, url_prefix="/login")
     db.create_all()"""
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
+    
