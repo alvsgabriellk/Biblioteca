e7733195bd064b1aca36d2fcc0d3090dcdf94e1f
alvsgabriellk <cauagabriell81@gmail.com>
2026-04-15 09:29:41 -0300

    Passando rotas de blueprint

--- routes/__init__.py
+++ routes/__init__.py
@@ -2,3 +2,4 @@ from .usuario_routes import usuario_bp
 from .livro_routes import livro_bp
 from .emprestimo_routes import emprestimo_bp
 from .cadastro_routes import cadastro_bp
+from .login_routes import login_bp
